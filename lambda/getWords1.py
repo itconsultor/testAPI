dbcec2dcea58f8a9b9b8f75a5fb865bc88dd1f3c
itconsultor <84429443+itconsultor@users.noreply.github.com>
2022-01-31 11:39:27 -0500

    Create getWords1.py

---
+++ lambda/getWords1.py
@@ -0,0 +1,26 @@
+import json
+import urllib3
+import boto3
+from uuid import uuid4
+
+client = boto3.client('apigatewaymanagementapi', endpoint_url="https://x01m0bei28.execute-api.us-east-1.amazonaws.com/production")
+dynamodb = boto3.resource("dynamodb")
+
+def lambda_handler(event, context):
+    
+    print(event)
+    
+    connectionId = event["requestContext"]["connectionId"]
+    
+    table = dynamodb.Table("TestCollection")
+    singleItem = table.scan(Limit=1, ExclusiveStartKey={'word': str(uuid4())})
+    
+    if singleItem['Items']:
+        response = client.post_to_connection(ConnectionId=connectionId, Data=json.dumps(singleItem))
+    else:
+        response = client.post_to_connection(ConnectionId=connectionId, Data=json.dumps("Didn't find an item. Please try again."))
+    
+    #print(singleItem)
+    
+    return { "statusCode": 200 }
+    #return None
